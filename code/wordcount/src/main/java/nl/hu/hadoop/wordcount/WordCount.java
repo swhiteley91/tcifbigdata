bb509b047c2cfd24ed670723f55639d330e8685c
Simon Whiteley <simonwhiteley@hotmail.com>
2016-02-15 10:21:04 +0100

    update

--- code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
+++ code/wordcount/src/main/java/nl/hu/hadoop/wordcount/WordCount.java
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
+import java.util.StringTokenizer;
 
 public class WordCount {
 
@@ -28,22 +29,31 @@ public class WordCount {
 	}
 }
 
-class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+class WordCountMapper extends Mapper<LongWritable, Text, Text, Text> {
 
-	public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
-		String[] tokens = value.toString().split("\\s");
-		for (String s : tokens) {
-			context.write(new Text(s), new IntWritable(1));
-		}
+	public void map(Text Key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString(), "\n");
+        while (itr.hasMoreTokens()) {
+            StringTokenizer itr2 = new StringTokenizer(itr.nextToken());
+            String website = itr2.nextToken();
+            Text websiteText = new Text();
+            websiteText.set(website);
+            while (itr2.hasMoreTokens()) {
+                Text keywordText = new Text(itr2.nextToken());
+                context.write(keywordText, websiteText);
+            }
+        }
 	}
 }
 
-class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-		int sum = 0;
-		for (IntWritable i : values) {
-			sum += i.get();
-		}
-		context.write(key, new IntWritable(sum));
+class WordCountReducer extends Reducer<Text, Text, Text, Text> {
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        StringBuilder output = new StringBuilder();
+        for(Text value : values) {
+            output.append(value.toString() + " ");
+        }
+        Text result = new Text();
+        result.set(output.toString());
+		context.write(key, result);
 	}
 }
