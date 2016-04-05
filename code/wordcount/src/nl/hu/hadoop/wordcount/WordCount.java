027d0e6a3325dbbcbf5fcffaeff752f902d9b3d3
Simon Whiteley <simonwhiteley@hotmail.com>
2016-04-05 20:03:24 +0200

    Letter erna totalen

--- code/wordcount/src/nl/hu/hadoop/wordcount/WordCount.java
+++ code/wordcount/src/nl/hu/hadoop/wordcount/WordCount.java
@@ -27,44 +27,60 @@ public class WordCount {
 		job.setReducerClass(WordCountReducer.class);
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 
 		job.waitForCompletion(true);
 	}
 }
 
-	class WordCountMapper extends Mapper<LongWritable, Text, Text, Text> {
+	class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 	public void map(LongWritable Key, Text value, Context context) throws IOException, InterruptedException {
 		Log log = LogFactory.getLog(WordCountMapper.class);
 
+		String[] alphabet = { "a", "b", "c", "d", "e", "f", "g",
+				"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+				"u", "v", "w", "x", "y", "z" };
+
 		int lineN = 0;
 
-		String[] parts = line.split(",");
+		String val = value.toString();
+		String[] parts = val.split(",");
 
 		log.info(parts[0]);
 		if(!parts[0].equals("Letter")) {
-			log.info(parts[0] + " DOESNT EQUAL LETTER");
 			int xTotal = 0;
 			int i = 0;
 			for(String part : parts) {
 				if(i != 0) {
-					log.info("adding " + Integer.parseInt(part.trim()) + " to " + xTotal);
-					xTotal += Integer.parseInt(part.trim());
+					//log.info("adding " + Integer.parseInt(part.trim()) + " to " + xTotal);
+					context.write(new Text(alphabet[i-1]), new IntWritable(Integer.parseInt(part.trim())));
+					//xTotal += Integer.parseInt(part.trim());
 				}
 				i += 1;
 			}
-			log.info("GOT AS XTOTAL: " + xTotal);
+
 		}
-		log.info("line: " + lineN);
 		lineN += 1;
-		log.info("HUUH LINE: " + lineN);
 
 	}
 }
 
-class WordCountReducer extends Reducer<Text, Text, Text, Text> {
-	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+class WordCountReducer extends Reducer<Text, IntWritable, Text, Text> {
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		System.out.println("KEY: " + key);
+		System.out.println(("VALUES: "));
+
+
+		String[] alphabet = { "a", "b", "c", "d", "e", "f", "g",
+				"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+				"u", "v", "w", "x", "y", "z" };
+
+		Integer total = 0;
+		for(IntWritable val : values) {
+			total += new Integer(val.toString());
+		}
+		context.write(key, new Text(total.toString()));
 
 	}
 }
